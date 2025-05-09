326c5ddd31fab1753df652bb43bdb69875e0f8d7
Vasyah <Souji_933@mail.ru>
2025-05-09 15:16:08 +0300

    fix: попытка реализовать загрузку изображений

--- src/features/HotelModal/lib/validation.ts
+++ src/features/HotelModal/lib/validation.ts
@@ -1,46 +1,31 @@
-import { z } from 'zod';
-import { HOTEL_TYPES } from './const';
+import { z } from 'zod'
+import { HOTEL_TYPES } from './const'
+
+const imageSchema = z.object({
+  id: z.string(),
+  file: z.instanceof(File),
+})
 
 export const hotelFormSchema = z.object({
-    id: z.string().optional(),
-    title: z.string().min(1, 'Название отеля обязательно для заполнения'),
-    type: z.object(
-        {
-            id: z.string(),
-            label: z.string(),
-        },
-        { required_error: 'Тип отеля обязателен для выбора' },
-    ),
-    rating: z.string().refine(
-        (val: string) => {
-            const num = Number(val);
-            return !isNaN(num) && num >= 1 && num <= 5;
-        },
-        { message: 'Рейтинг должен быть числом от 1 до 5' },
-    ),
-    address: z.string().min(1, 'Адрес обязателен для заполнения'),
-    telegram_url: z
-        .string()
-        .url('Неверный формат URL')
-        .refine((val: string) => val.startsWith('https://t.me/'), {
-            message: 'URL должен начинаться с https://t.me/',
-        })
-        .optional(),
-    phone: z.string().min(1, 'Номер телефона обязателен для заполнения'),
-    user_id: z.object(
-        {
-            id: z.string(),
-            label: z.string(),
-        },
-        { required_error: 'Отельер обязателен для выбора' },
-    ),
-    description: z.string().default(''),
-    image_id: z
-        .object({
-            id: z.string(),
-            file: z.instanceof(File),
-        })
-        .optional(),
-});
+  id: z.string().optional(),
+  title: z.string().min(1, 'Название отеля обязательно для заполнения'),
+  type: z.object({
+    id: z.string(),
+    label: z.string(),
+  }),
+  rating: z.string().refine(val => {
+    const num = Number(val)
+    return !isNaN(num) && num >= 1 && num <= 5
+  }, 'Рейтинг должен быть от 1 до 5'),
+  user_id: z.object({
+    id: z.string(),
+    label: z.string(),
+  }),
+  address: z.string().min(1, 'Адрес обязателен для заполнения'),
+  phone: z.string().min(1, 'Номер телефона обязателен для заполнения'),
+  telegram_url: z.string().optional(),
+  description: z.string().default(''),
+  image_id: imageSchema.nullable().optional(),
+})
 
-export type HotelFormSchema = z.infer<typeof hotelFormSchema>;
+export type HotelFormSchema = z.infer<typeof hotelFormSchema>
--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -1,21 +1,34 @@
-import React, { ComponentProps, CSSProperties, FC, useCallback, useEffect, useState } from 'react';
-import { Loader as LoaderConsta } from '@consta/uikit/Loader';
-import cx from './style.module.scss';
-import cn from 'classnames';
+import React, {
+  ComponentProps,
+  CSSProperties,
+  FC,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
+import { Loader as LoaderConsta } from '@consta/uikit/Loader'
+import cx from './style.module.scss'
+import cn from 'classnames'
 
 export interface LoaderProps extends ComponentProps<typeof LoaderConsta> {
-    style?: CSSProperties;
-    className?: string;
+  style?: CSSProperties
+  className?: string
 }
 
 export const Loader: FC<LoaderProps> = ({ style, ...props }) => {
-    return <LoaderConsta className={cx.loader} type={'dots'} size={'m'} {...props} />;
-};
+  return (
+    <LoaderConsta className={cx.loader} type={'dots'} size={'m'} {...props} />
+  )
+}
 
-export const FullWidthLoader: FC<LoaderProps> = ({ style, className, ...props }) => {
-    return (
-        <div className={cn(cx.container, className)}>
-            <Loader />
-        </div>
-    );
-};
+export const FullWidthLoader: FC<LoaderProps> = ({
+  style,
+  className,
+  ...props
+}) => {
+  return (
+    <div className={cn(cx.container, className)}>
+      <Loader />
+    </div>
+  )
+}
