558ca5b3c673ecb14808c9d182db7d5d01097b5a
Vasyah <Souji_933@mail.ru>
2025-11-15 20:37:53 +0300

    ISSUE-49: изменил цвет текста и размер добавления номеров

--- src/features/RoomInfo/ui/RoomModal.tsx
+++ src/features/RoomInfo/ui/RoomModal.tsx
@@ -79,6 +79,7 @@ export const RoomModal: FC<RoomModalProps> = ({
                     isEdit={isEdit}
                 />
             }
+            descriptionClassName={'max-w-lg'}
         />
     );
 };
